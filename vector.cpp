a1a335107ff5b6976d928fbd602de610ccbd0eb0
teodor <teodorilie007@gmail.com>
2025-12-26 16:46:06 +0200

    V0.2.11 joburi

--- vector.cpp
+++ vector.cpp
@@ -4,6 +4,18 @@
 
 vector::vector(float val_x, float val_y) : x(val_x), y(val_y) {}
 
+vector::vector(const vector& other) : x(other.x), y(other.y) {}
+
+vector& vector::operator=(const vector& other)
+{
+    if (this != &other)
+    {
+        x = other.x;
+        y = other.y;
+    }
+    return *this;
+}
+
 float vector::getx() const 
 { 
     return x;
